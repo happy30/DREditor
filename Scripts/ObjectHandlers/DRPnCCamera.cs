0845f5a121a7b32a0e0ba053655b37ea8e7b1fbf
SS <gianmiradora@yahoo.com>
2019-09-16 14:43:47 +0800

    Improved DRPnCCamera script
    Changes:
    -Renamed Variables for clarity.
    -Added Vector3 CharPosition as the default position.
    -Added InitialHAngle and InitialVAngle.
    -Utilized previously unused CamSpeed variable.
    -Added TargetAimRotation and TargetCamDistance for when focusing on an object.

--- Scripts/ObjectHandlers/DRPnCCamera.cs
+++ Scripts/ObjectHandlers/DRPnCCamera.cs
@@ -2,81 +2,96 @@
 using UnityEngine;
 using System.Collections;
 
-namespace DREditor.Handlers
+namespace DREditor.Camera
 {
-    [AddComponentMenu("DREditor/Handlers/DRPnCCamera")]
+    [AddComponentMenu("DREditor/Cameras/DRPnCCamera")]
     public class DRPnCCamera : MonoBehaviour
     {
+        //Public parameters
+        public bool Controllable = true;
+        public Vector3 CharPosition = new Vector3(0f, 1.8f, 0f);
+        public float MaxHAngle = 30f;
+        public float MaxVAngle = 20f;
+        public float CamSpeed = 3f;
+        public float InitialHAngle = 0f;
+        public float InitialVAngle = 0f;
+        public float CamDistance = 8f;
 
-        public bool controlEnabled;
-        int charPosition;
-        float camAngle;
-        float camVAngle;
-        public float maxAngle = 30f;
-        public float maxVAngle = 20f;
-        public float camSpeed = 1.5f;
+        //Focus on Object
+        public GameObject TargetObject;
+        public Vector3 TargetAimRotation;
+        public float TargetCamDistance = 5f;
 
-        public Vector3 targetFocus = new Vector3(0f, 1.8f, 0f);
-        public GameObject targetChar;
+        //Private variables for calculation
+        private float CamHAngle, CamVAngle;
+        private Vector3 TargetFocus;
+        private Quaternion InitAngle;
+        private Quaternion FinalAimRotation;
+        private Vector3 FinalCamPosition;
 
-        public Vector3 finalCamPosition;
-        public float camDistance;
-
-        Quaternion finalAimRotation;
+        void Start()
+        {
+            InitAngle = Quaternion.Euler(InitialVAngle, InitialHAngle, 0f);
+        }
 
         void Update()
         {
-            if (controlEnabled)
+            //Please comment Line 35 if your desired inital angles is already applied.
+            //InitAngle = Quaternion.Euler(InitialVAngle, InitialHAngle, 0f);
+
+            if (Controllable)
             {
-                camAngle += Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1) * 30 * Time.deltaTime;
-                camVAngle += Mathf.Clamp(Input.GetAxis("Vertical"), -1, 1) * 30 * Time.deltaTime;
-
-                camAngle = Mathf.Clamp(camAngle, -maxAngle, maxAngle);
-                camVAngle = Mathf.Clamp(camVAngle, -maxVAngle, maxVAngle);
-
                 //Debug.Log("3DVNCamera: Currently Controllable!");
-                finalAimRotation = Quaternion.Euler(camVAngle, -camAngle, 0);
+                TargetObject = null;
+                TargetFocus = CharPosition;
 
-                if (targetChar != null)
-                {
-                    targetFocus = targetChar.transform.position;
-                }
-                finalCamPosition = (targetFocus - new Vector3(0, camVAngle / 30f)) - (transform.forward * camDistance);
+                CamHAngle += Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1) * (CamSpeed * 10) * Time.deltaTime;
+                CamVAngle += Mathf.Clamp(Input.GetAxis("Vertical"), -1, 1) * (CamSpeed * 10) * Time.deltaTime;
+                CamHAngle = Mathf.Clamp(CamHAngle, -MaxHAngle, MaxHAngle);
+                CamVAngle = Mathf.Clamp(CamVAngle, -MaxVAngle, MaxVAngle);
+
+                FinalAimRotation = Quaternion.Euler(CamVAngle + InitialVAngle, -CamHAngle + InitialHAngle, 0);
+                FinalCamPosition = (TargetFocus - new Vector3(0, CamVAngle / 30f)) - (transform.forward * CamDistance);
             }
             else
             {
-                if (targetChar != null)
+                if (TargetObject != null)
                 {
                     //Debug.Log("3DVNCamera: Currently Focusing!");
-                    targetFocus = targetChar.transform.position;
-                    finalAimRotation = Quaternion.Lerp(transform.rotation, targetChar.transform.rotation, Time.deltaTime * 10f);
-                    finalCamPosition = Vector3.Lerp(transform.position, targetFocus, Time.deltaTime * 10f);
+                    TargetFocus = TargetObject.transform.position;
+                    FinalAimRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(TargetAimRotation), Time.deltaTime * 30f);
+                    FinalCamPosition = Vector3.Lerp(transform.position, TargetFocus - (transform.forward * TargetCamDistance), Time.deltaTime * 30f);
                 }
             }
         }
 
-        public void ChangeCamPosition(string TargetCharacter)
+        public void FocusCamera(string TarObj)
         {
-            targetChar = GameObject.Find(TargetCharacter);
-            Debug.Log("3DVNCamera: Focusing on " + targetChar.name + "using a string.");
+            Controllable = false;
+            TargetObject = GameObject.Find(TarObj);
+            TargetAimRotation = Vector3.zero;
+            TargetCamDistance = 8f;
+            Debug.Log("3DVNCamera: Focusing on " + TargetObject.name + "using a string.");
         }
 
-        public void ChangeCamPosition(Vector3 targetPosition, Vector3 targetRotation)
+        public void FocusCamera(GameObject target, Vector3 rotation, float distance)
         {
-            targetFocus = targetPosition;
-            finalAimRotation = Quaternion.Euler(targetRotation);
+            Controllable = false;
+            TargetObject = target;
+            TargetAimRotation = rotation;
+            TargetCamDistance = distance;
             Debug.Log("3DVNCamera: Focusing on a spot manually.");
         }
 
         public void ControlSwitch(bool option)
         {
-            controlEnabled = option;
+            Controllable = option;
         }
 
         private void LateUpdate()
         {
-            transform.rotation = finalAimRotation;
-            transform.position = finalCamPosition;
+            transform.rotation = FinalAimRotation;
+            transform.position = FinalCamPosition;
         }
     }
 }
