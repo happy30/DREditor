f7e7018f502380d74bdcc4c876517a2be32282c0
Jarrett Johnson <jarrettsjohnson@gmail.com>
2020-05-06 00:36:02 -0400

    Present Editor

---
+++ Scripts/PresentEditor/Editor/PresentEditor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using DRUtility = DREditor.Utility.Editor.HandyFields;
+
+namespace DREditor.PresentEditor
+{
+    [CustomEditor(typeof(Present))]
+    public class PresentEditor : Editor
+    {
+        private Present pres;
+        private void OnEnable()
+        {
+            pres = (Present)target;
+        }
+        public override void OnInspectorGUI()
+        {
+            Label("Present");
+
+            CreateForm();
+            EditorUtility.SetDirty(pres);
+        }
+        private void Label(string label)
+        {
+            GUI.backgroundColor = Color.white;
+            var labelStyle = new GUIStyle();
+            labelStyle.fontSize = 10;
+            GUILayout.Label(label, labelStyle);
+        }
+        private void CreateForm()
+        {
+            EditorGUILayout.BeginVertical("Box");
+            pres.Name = DRUtility.StringField("Name: ", pres.Name);
+            pres.index = DRUtility.IntField("No. ", pres.index);
+            GUILayout.Label("Description:");
+            pres.Description = EditorGUILayout.TextArea(pres.Description, GUILayout.Height(75), GUILayout.Width(Screen.width - 310));
+            pres.image = DRUtility.UnityField("Image: ", pres.image);
+
+            for (int i = 0; i < pres.CharacterReactions.Count; i++)
+            {
+                var reaction = pres.CharacterReactions[i];
+
+                EditorGUILayout.BeginHorizontal("Box");
+
+                EditorGUILayout.BeginVertical();
+                GUIStyle expr = new GUIStyle();
+                expr.normal.background = reaction.character.DefaultSprite;
+                EditorGUILayout.LabelField(GUIContent.none, expr, GUILayout.Width(100), GUILayout.Height(100));
+                reaction.character = DRUtility.UnityField("Reaction: ", reaction.character, 120, 20);
+                EditorGUILayout.EndVertical();
+
+                // Select Reaction
+                GUILayout.BeginVertical();
+                GUILayout.FlexibleSpace();
+                reaction.reactionLevel = (PresentReactionLevel)EditorGUILayout.EnumPopup(reaction.reactionLevel);
+                GUILayout.FlexibleSpace();
+                GUILayout.EndVertical();
+
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.EndVertical();
+            if (GUILayout.Button("Add New Character Reaction", GUILayout.Width(200)))
+            {
+                pres.CharacterReactions.Add(new StudentReactions());
+            }
+        }
+    }
+}
--- Scripts/Utility/Editor/HandyFields.cs
+++ Scripts/Utility/Editor/HandyFields.cs
@@ -8,16 +8,28 @@ namespace DREditor.Utility.Editor
     /// <summary>
     /// Class with shorthand methods for Editor GUI layout.
     /// </summary>
-    public class HandyFields
+    public static class HandyFields
     {
-        public static Texture2D TextureField(Texture2D texture)
+        public static Texture2D TextureField(Texture2D texture, int width = 120, int height = 120)
         {
             GUILayout.BeginVertical();
             var style = new GUIStyle(GUI.skin.label);
 
             style.fixedWidth = 70;
-            var result = (Texture2D)EditorGUILayout.ObjectField(texture, typeof(Texture2D), false, GUILayout.Width(120), GUILayout.Height(120));
+            var result = (Texture2D)EditorGUILayout.ObjectField(texture, typeof(Texture2D), false, GUILayout.Width(width), GUILayout.Height(height));
             GUILayout.EndVertical();
+            return result;
+        }
+
+        public static int IntField(string name, int value, int textBoxWidth = 200)
+        {
+            GUI.backgroundColor = Color.white;
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(name, GUILayout.Width(80));
+
+            var result = EditorGUILayout.IntField(value, GUILayout.Width(textBoxWidth));
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
             return result;
         }
 
@@ -28,6 +40,18 @@ namespace DREditor.Utility.Editor
             var style = new GUIStyle(GUI.skin.label);
             style.fixedWidth = 70;
             var result = (Sprite)EditorGUILayout.ObjectField(sprite, typeof(Sprite), false, GUILayout.Width(120), GUILayout.Height(120));
+            GUILayout.EndVertical();
+            return result;
+        }
+
+        public static T UnityField<T>(string name, T data, int width = 120, int height = 120) where T : Object
+        {
+            GUILayout.BeginVertical();
+            var style = new GUIStyle(GUI.skin.label);
+            //GUILayout.Label(name, GUILayout.Width(80));
+
+            //style.fixedWidth = 70;
+            var result = (T)EditorGUILayout.ObjectField(data, typeof(T), false, GUILayout.Width(width), GUILayout.Height(height));
             GUILayout.EndVertical();
             return result;
         }
