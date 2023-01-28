38e6cd3d5d09a7f698bd122e8feb5535ac67932d
CertifiedSharp <morebj1000@gmail.com>
2023-01-27 19:00:58 -0800

    Fixing CharTweener Issues when importing

--- DependencyPlugins/CharTweener-master/Assets/CharTween/Scripts/CharTweenerUtility.cs
+++ DependencyPlugins/CharTweener-master/Assets/CharTween/Scripts/CharTweenerUtility.cs
@@ -5,21 +5,20 @@ namespace CharTween
 {
     public static class CharTweenerUtility
     {
-        private static readonly Dictionary<TMP_Text, CharTweener> Tweeners = new Dictionary<TMP_Text, CharTweener>();
+        private static readonly Dictionary<TMP_Text, CharTweener> CharModifiers = new Dictionary<TMP_Text, CharTweener>();
 
         /// <summary>
         /// Returns a <see cref="CharTweener"/> guaranteeing the same instance is used for the same text.
         /// </summary>
         public static CharTweener GetCharTweener(this TMP_Text text)
         {
-            CharTweener tweener;
-            if (Tweeners.TryGetValue(text, out tweener))
-                return tweener;
+            if (CharModifiers.ContainsKey(text))
+                return CharModifiers[text];
 
-            tweener = text.gameObject.AddComponent<CharTweener>();
-            tweener.Initialize(text);
-            Tweeners.Add(text, tweener);
-            return tweener;
+            var modifier = CharModifiers[text] = text.gameObject.AddComponent<CharTweener>();
+            modifier.Text = text;
+            modifier.Initialize();
+            return modifier;
         }
     }
 }
