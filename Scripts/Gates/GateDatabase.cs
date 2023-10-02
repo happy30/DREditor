680d4d7c31e08de0a1a895e83be8a068a0bbbf2e
Benjamin Jillson <55756187+CertifiedSharp@users.noreply.github.com>
2023-10-02 16:36:43 -0500

    Update GateDatabase.cs

--- Scripts/Gates/GateDatabase.cs
+++ Scripts/Gates/GateDatabase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DREditor.Gates
@@ -24,6 +25,42 @@ namespace DREditor.Gates
             UnityEngine.Debug.LogWarning("Gateway could not be found for " + scene + 
                 "! Make sure that the name spelling is correct and that the scene" +
                 " has been referenced in the unity builder! ");
+            return null;
+        }
+        public bool Contains(string scene)
+        {
+            foreach (Gate a in Areas)
+            {
+                if (a != null && a.toAreaName == scene)
+                    return true;
+            }
+            return false;
+        }
+        public string[] GetGateNames()
+        {
+            List<string> names = new List<string>();
+            foreach(Gate g in Areas)
+            {
+                names.Add(g.toAreaName);
+            }
+            return names.ToArray();
+        }
+        public bool IsInvGate(Gate g)
+        {
+            foreach(Gate gate in Areas)
+            {
+                if(gate.invGates.Where(n => n.sceneName == g.toAreaName).ToArray().Length > 0)
+                    return true;
+            }
+            return false;
+        }
+        public List<GateFrom> GetParentLocationList(Gate g)
+        {
+            foreach(Gate gate in Areas)
+            {
+                if (gate.invGates.Where(n => n.sceneName == g.toAreaName).ToArray().Length > 0)
+                    return gate.fromLocationList;
+            }
             return null;
         }
     }
